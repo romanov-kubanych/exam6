313e1a1820c106fb94a3c091a347bc8ce8b82548
captainQ <lordkuvin@gmail.com>
2022-01-08 13:36:42 +0600

    Реализована редактирование и удаление записи

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from webapp.views import index_view, create_book_view
+from webapp.views import index_view, create_book_view, update_book_view, delete_book_view
 
 urlpatterns = [
     path('', index_view, name='index_view'),
-    path('book/add/', create_book_view, name='create_view')
+    path('book/add/', create_book_view, name='create_view'),
+    path('book/<int:pk>/update/', update_book_view, name='update_view'),
+    path('book/<int:pk>/delete/', delete_book_view, name='delete_view')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.forms import BookForm
 from webapp.models import Book
@@ -23,3 +23,34 @@ def create_book_view(request):
             books = Book.objects.all().filter(status='active').order_by('-created_at')
             return render(request, 'index_view.html', {'books': books})
         return render(request, 'book_create.html', {"form": form})
+
+
+def update_book_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'GET':
+        form = BookForm(initial={
+            'author': book.author,
+            'email': book.email,
+            'text': book.text
+        })
+        return render(request, 'book_update.html', {'form': form, 'book': book})
+    else:
+        form = BookForm(data=request.POST)
+        if form.is_valid():
+            book.author = form.cleaned_data.get('author')
+            book.email = form.cleaned_data.get('email')
+            book.text = form.cleaned_data.get('text')
+            book.save()
+            books = Book.objects.all().filter(status='active').order_by('-created_at')
+            return render(request, 'index_view.html', {'books': books})
+        return render(request, 'book_update.html', {'form': form, 'book': book})
+
+
+def delete_book_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'book_delete.html', {'book': book})
+    else:
+        book.delete()
+        books = Book.objects.all().filter(status='active').order_by('-created_at')
+        return render(request, 'index_view.html', {'books': books})
